9e23ebb51dedff535c88775669638885b485a057
Matt Scott <mattscott251@gmail.com>
2023-08-11 15:15:33 -0500

    completed first problem

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,3 +1,13 @@
-module.exports = function reverseString(string) {
-  // Your code here
+function reverseString(string) {
+  if (typeof string !== 'string') {
+    throw TypeError('Argument must be a string!');
+  }
+
+  let newString = ('');
+  for (let i = string.length - 1; i >= 0; i--) {
+    newString += string[i];
+  }
+  return newString;
 };
+
+module.exports = reverseString;
--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,16 @@
-// Your code here
+const chai = require('chai');
+const expect = chai.expect;
+
+const reverseString = require('../problems/reverse-string');
+
+describe('reverseString', () => {
+    it ('should return the reverse of the entered string', () => {
+        let newString1 = reverseString("fun");
+        expect(newString1).to.equal('nuf');
+    });
+
+    it ('should throw a TypeError when argument passed to function not a string', () => {
+        //expect function to throw an error when passed a particular argument.
+        expect(function(){reverseString(5)}).to.throw(TypeError);
+    });
+});
